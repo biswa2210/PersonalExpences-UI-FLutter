16a960abd2366346735d27b81a123a770ce80b5b
Biswarup Bhattacharjee <74323889+biswa2210@users.noreply.github.com>
2021-05-16 12:03:05 +0530

    Update user_transactions.dart

--- lib/widgets/user_transactions.dart
+++ lib/widgets/user_transactions.dart
@@ -2,6 +2,11 @@ import 'package:flutter/material.dart';
 import 'package:personal_expenses/models/transaction.dart';
 import 'package:personal_expenses/widgets/new_transactions.dart';
 import 'package:personal_expenses/widgets/transaction_list.dart';
+/*
+CREATED BY BISWARUP BHATTACHARJEE
+EMAIL    : bbiswa471@gmail.com
+PHONE NO : 6290272740
+*/
 class UserTransactions extends StatefulWidget {
   @override
   _UserTransactionsState createState() => _UserTransactionsState();
